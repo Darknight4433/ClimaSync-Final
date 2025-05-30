8b5cf99ddac1a34d6fb3685a52c5c399fda23fc9
Darknight4433 <vaishnavil4433@gmail.com>
2025-05-30 14:59:02 +0530

    Add dependencies for deployment

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
-const express = require("express");
-const axios = require("axios");
-require("dotenv").config();
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static("public"));
-
-app.get("/weather", async (req, res) => {
-  const city = req.query.city;
-  try {
-    const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=metric`
-    );
-    res.json(response.data);
-  } catch (error) {
-    res.json({ error: "City not found" });
+{
+  "name": "climasync",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^5.1.0",
+    "axios": "^1.4.0",
+    "dotenv": "^16.0.3"
   }
-});
-
-app.listen(PORT, () => {
-  console.log(`ClimaSync server running at http://localhost:${PORT}`);
-});
+}
